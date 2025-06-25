05589761f5db8e1e3432b4030d1bd321536771b2
Mint <audwl03071@gmail.com>
2025-06-26 01:29:33 +0900

    fix: 예약 대기 취소 응답에 id 추가

--- src/main/java/roomescape/dto/response/reservation/MyReservationRetrievalResponse.java
+++ src/main/java/roomescape/dto/response/reservation/MyReservationRetrievalResponse.java
@@ -1,22 +1,14 @@
 package roomescape.dto.response.reservation;
 
-import roomescape.domain.reservation.Reservation;
-import roomescape.domain.reservation.ReservationStatus;
-import roomescape.domain.slot.Slot;
-
 public record MyReservationRetrievalResponse(
+        Long waitingId,
         String theme,
         String date,
         String time,
         String status
 ) {
-    public static MyReservationRetrievalResponse from(Reservation reservation) {
-        Slot slot = reservation.getSlot();
-        return new MyReservationRetrievalResponse(
-                slot.getTheme().getName().getName(),
-                slot.getDate().getDate().toString(),
-                slot.getTime().getStartAt().toString(),
-                ReservationStatus.RESERVED.getViewName()
-        );
+    public static MyReservationRetrievalResponse from(final MyReservationAndWaitingSortedResult result) {
+        return new MyReservationRetrievalResponse(result.waitingId(), result.theme(), result.date().toString(), result.time().toString(),
+                result.status());
     }
 }
